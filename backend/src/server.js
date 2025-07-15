e883a64107bffd40c7584ca2881153b8b7a2cdca
Fraz Baig <ab15401556@gmail.com>
2025-07-15 13:32:50 +0500

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,15 +5,19 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middlewares/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 dotenv.config();
 
-
+const __dirname = path.resolve()
 
 // const express = require("express");
 const app=express();
-app.use(cors({
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin:"http://localhost:5173"
 }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -23,6 +27,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes);
 app.use("/api/products",productRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 const PORT = process.env.PORT || 5001;
 
 
